1d5db97d5136b27eef5c0ebf6c5b48cfb77da907
Martikainen Aleksi <aleksi.martikainen@nokia.com>
2020-07-04 12:58:21 +0300

    Add ambient sounds

---
+++ Assets/Scripts/AudioFadeOut.cs
@@ -0,0 +1,33 @@
+﻿    using UnityEngine;
+    using System.Collections;
+     
+    public static class AudioFadeOut 
+    { 
+        // Fade out
+        public static IEnumerator FadeOut (AudioSource audioSource, float FadeTime) 
+        {
+            float startVolume = audioSource.volume;
+            while (audioSource.volume > 0) 
+            {
+                audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
+                yield return null;
+            }
+            audioSource.Stop ();
+            audioSource.volume = startVolume;
+        }
+
+        // Fade in
+        public static IEnumerator FadeIn (AudioSource audioSource, float FadeTime) 
+        {
+            float startVolume = audioSource.volume;
+            audioSource.Play();
+            audioSource.volume = 0;
+            while (audioSource.volume <= 0.99f) 
+            {
+                audioSource.volume += startVolume * Time.deltaTime / FadeTime;
+                yield return null;
+            }     
+
+        }
+    }
+     
