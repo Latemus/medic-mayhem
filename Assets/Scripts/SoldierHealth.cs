b3178cdeb41343d2701ada1d413e5417d6e5b4a5
taturl <t>
2020-07-05 00:25:53 +0300

    Add wounded soldier spawn after falling

--- Assets/Scripts/SoldierHealth.cs
+++ Assets/Scripts/SoldierHealth.cs
@@ -3,5 +3,21 @@
  
  public class SoldierHealth : MonoBehaviour { 
     public int curHealth = 5;
+
+    public GameObject becomesAfterDying;
+
+    private Vector3 dyingPosition;
+
+    void Update()
+    {
+        dyingPosition = transform.position;
+    }
+
+    void OnDestroy()
+    {
+        int faceUpOrDown = Random.Range (0, 2);
+        GameObject new_soldier = Instantiate(becomesAfterDying, dyingPosition,
+            Quaternion.Euler(new Vector3(90 + faceUpOrDown*180, 0, 0)));
+    }
     
 }
