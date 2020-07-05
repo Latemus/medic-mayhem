e7a007e349951090a33659ff623cc1af8e0244b5
Martikainen Aleksi <aleksi.martikainen@nokia.com>
2020-07-05 13:50:24 +0300

    Add wintextanimation logic

--- Assets/Scripts/WinTextAnimation.cs
+++ Assets/Scripts/WinTextAnimation.cs
@@ -8,6 +8,7 @@ public class WinTextAnimation : MonoBehaviour
     public Vector3 end_location = new Vector3(0,10,0);
     private AudioSource audioSource;
     private bool have_already_played = false;
+    public bool victory = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,16 +17,18 @@ public class WinTextAnimation : MonoBehaviour
     }
 
 
-
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.position = Vector3.MoveTowards(transform.position, end_location, .3f);
-        Vector3 distance_vector = transform.position - end_location;
-        if (distance_vector.sqrMagnitude < 1 && !audioSource.isPlaying && !have_already_played) 
+        if (victory) 
         {
-            have_already_played = true;
-            audioSource.Play();
+            transform.position = Vector3.MoveTowards(transform.position, end_location, .3f);
+            Vector3 distance_vector = transform.position - end_location;
+            if (distance_vector.sqrMagnitude < 1 && !audioSource.isPlaying && !have_already_played) 
+            {
+                have_already_played = true;
+                audioSource.Play();
+            }
         }
     }
 }
