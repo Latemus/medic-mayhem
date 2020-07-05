cf49bd9821f8ca4d694f4dab2617c7cbd2d30763
Tatu Heinämäki <tatu.heinamaki@gmail.com>
2020-07-05 22:50:30 +0300

    post processing assets added, menu images added, settings tweaked

---
+++ Assets/Scripts/MenuImageDecider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuImageDecider : MonoBehaviour
+{
+
+    public GameObject[] ObjectsList; // Not only 18, this script will except any number of game objects you put here.
+     
+    void Start()
+    {
+    ObjectsList[Random.Range(0,ObjectsList.Length)].SetActive(true);
+    this.enabled = false;
+    }
+
+
+}
